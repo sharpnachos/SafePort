db3b6d7a4736ed0e5edaa4b5ebd597614b45b2f6
Eric <1eric.hoffman@gmail.com>
2019-04-09 11:58:39 -0500

    finished scan page

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, titleBarStyle: 'hidden'})
+  win = new BrowserWindow({ width: 800, height: 600, titleBarStyle: 'hidden', resizable: false })
 
   // and load the index.html of the app.
   win.loadFile('src/html/index.html')
