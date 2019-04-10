96d1e57bfda51d26d18c6c84afd1633b8e8aa0f4
JoeWolke <46971617+JoeWolke@users.noreply.github.com>
2019-04-10 11:20:33 -0500

    TestOpenPort.py
    Tests if the correct values are returned if the port is open.

---
+++ src/python/TestOpenPort.py
@@ -0,0 +1,30 @@
+#TestOpenPort
+
+import unittest
+import tempFull
+import threading
+
+
+class OpenPort(unittest.TestCase):
+#Test for open ports. We need to make the test name as descriptive as possible
+#self tells us that known values are going to be running the test
+	
+
+	def test_for_open_port_verification(self):
+		
+		#Make sure that if the port is open, it returns the correct value
+		self.assertTrue(tempFull,('is open!'))
+		
+	def test2_for_open_port_verification(self):
+		self.assertNotEqual(tempFull,('is closed'))
+		
+  #This tests threading, but I can't it to work yet
+  
+	#def test_threading(self):
+  #t = threading.Thread(target=lambda: self.assertTrue(False))
+  #t.start()
+  #t.join()	
+		
+if __name__ == '__main__':
+	unittest.main()
+
